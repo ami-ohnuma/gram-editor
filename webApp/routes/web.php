232e56e7d8b4f83bb6e9b6c8f612ff16866f772b
hidekiohnuma <h-onuma@advanced-media.co.jp>
2024-04-10 16:44:25 +0900

    ログイン処理実装

--- webApp/routes/web.php
+++ webApp/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    if (Auth::check()) {
+        // ログイン済みのときの処理
+        return view('home');
+    } else {
+        // ログインしていないときの処理
+        return view('auth/login');
+    }
+    // return view('welcome');
 });
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
